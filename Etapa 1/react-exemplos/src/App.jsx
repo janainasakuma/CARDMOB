9f01917512bd3be0d3c7fcaaf4dd841d40be2818
janainasakuma <sakumajanaina@gmail.com>
2025-03-11 09:55:36 -0300

    add Photo component

--- Etapa 1/react-exemplos/src/App.jsx
+++ Etapa 1/react-exemplos/src/App.jsx
@@ -1,12 +1,38 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
 import Counter from './components/Counter';
+import Photo from './components/Photo';
 
 function App() {
   const [count, setCount] = useState(0);
+  const [photos, setPhotos] = useState([]);
+
+  const fetchPhotos = async () => {
+    try {
+      const url = 'https:/jsonplaceholder.typicode.com/albums/1/photos';
+      const response = await fetch(url); //por padrão executa um request do tipo GET
+      if (response.status === 200) {
+        const data = await response. json()
+        const updatedPhotos = data.map((photo) => ({
+          ...photo,
+            thumbnailUrl: `https://picsum.photos/150?random=${photo.id}`
+          }));
+        setPhotos(updatedPhotos);
+      }
+
+
+    } catch (error) {
+      console.error('Erro ao buscar fotos', error);
+    }
+  }
+
+  useEffect(() => {
+    fetchPhotos();
+  }, [])
+
 
   // function updateCount() {
   //   setCount(count+1);
@@ -39,6 +65,18 @@ function App() {
     <>
       <Counter title="Contando..." />
       <Counter initial="100" />
+      <article>
+        <h1>Album da API</h1>
+        {photos.map((photo) => {
+          // return (
+          //   <article key={photo.id}>
+          //     <h2>ID #{photo.id} {photo.title}</h2>
+          //     <img src={photo.thumbnailUrl} alt={photo.title} />
+          //   </article>
+          //)
+          <Photo photo={photo}/>
+        })}
+      </article>
     </>
   )
 }
