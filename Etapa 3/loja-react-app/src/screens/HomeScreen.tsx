0e1295798716cfe33e9962e5d19768e2473eb0fc
janainasakuma <sakumajanaina@gmail.com>
2025-08-12 09:06:57 -0300

    Inclui ThemeContext

--- Etapa 3/loja-react-app/src/screens/HomeScreen.tsx
+++ Etapa 3/loja-react-app/src/screens/HomeScreen.tsx
@@ -1,11 +1,14 @@
 import React from "react";
-import { View, Text } from "react-native";
+import { View, Text, Button, StyleSheet } from "react-native";
 
 function HomeScreen() {
-
+    const { theme, toggleTheme } = useTheme();
     return (
-        <View>
-            <Text>Home Screen</Text>
+        <View style={[styles.container, { backgroundColor: theme.colors.background }]}>
+            <Text style={[ color: theme.colors.text, marginBottom: theme.spacing(1) ]}>
+                Home Screen
+                </Text>
+            <Button title="Alteranar Tema" color={theme.colors.primary} onPress={toggleTheme}/>
         </View>
     );
 }
